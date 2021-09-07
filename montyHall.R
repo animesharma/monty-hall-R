99e69a69136b93375442238d18814e31cd00033a
Vipul Gharde <vipul.gharde@gmail.com>
2021-09-06 22:32:17 -0400

    Reformatted Code

--- montyHall.R
+++ montyHall.R
@@ -1,8 +1,8 @@
-# Function to return a door that is neither the value passed to 
+# Function to return a door that is neither the value passed to
 # tempDoor nor the door chosen by the player
 getDoor <- function (tempDoor, chosenDoor, numDoors) {
   i <- 1
-  while (i == tempDoor || i == chosenDoor) 
+  while (i == tempDoor || i == chosenDoor)
     i <- (i + 1) %% numDoors
   return(i)
 }
@@ -33,8 +33,8 @@ montyHallSimulator <- function (doors, doorSwitched, runs) {
     if (doorSwitched == TRUE) {
       chosenDoor <- getDoor(revealedDoor, chosenDoor, numDoors)
     }
-    # Increment the respective counts of the cases based on whether or 
-    # not the chosen door is the prize door and if the door was switched 
+    # Increment the respective counts of the cases based on whether or
+    # not the chosen door is the prize door and if the door was switched
     # by the player after the first (non-prize) door reveal
     if (chosenDoor == prizeDoor && doorSwitched == FALSE) {
       counts$winNoSwitch <- counts$winNoSwitch + 1
@@ -64,12 +64,12 @@ iterations = 1000
 # Run Monty Hall simulation.
 switchRes <- vector()
 noSwitchRes <- vector()
-for(i in 1:iterations) {
+for (i in 1:iterations) {
   # switchRes holds the result for the case when the player switches midway
-  switchRes <- append(switchRes, 
+  switchRes <- append(switchRes,
                       montyHallSimulator(doors, TRUE, runs))
   # noSwitchRes holds the result for the case when the player doesn't switch
-  noSwitchRes <- append(noSwitchRes, 
+  noSwitchRes <- append(noSwitchRes,
                         montyHallSimulator(doors, FALSE, runs))
 }
 
@@ -77,7 +77,7 @@ for(i in 1:iterations) {
 tempWinSwitch <- vector()
 tempLoseSwitch <- vector()
 i <- 1
-while(i <= length(switchRes)) {
+while (i <= length(switchRes)) {
   tempWinSwitch <- append(tempWinSwitch, switchRes[i])
   tempLoseSwitch <- append(tempLoseSwitch, switchRes[i + 2])
   i = i + 4
@@ -85,40 +85,47 @@ while(i <= length(switchRes)) {
 tempWinNoSwitch <- vector()
 tempLoseNoSwitch <- vector()
 i <- 1
-while(i <= length(noSwitchRes)) {
+while (i <= length(noSwitchRes)) {
   tempWinNoSwitch <- append(tempWinNoSwitch, noSwitchRes[i + 1])
   tempLoseNoSwitch <- append(tempLoseNoSwitch, noSwitchRes[i + 3])
   i = i + 4
 }
 
-# Create dataframes containing outcomes for cases when the player 
+# Create dataframes containing outcomes for cases when the player
 # switches the door and does not switch the door
 switch <- data.frame("Winning" = unlist(tempWinSwitch),
                      "Losing" = unlist(tempLoseSwitch))
-noSwitch <- data.frame("Winning" = unlist(tempWinNoSwitch), 
+noSwitch <- data.frame("Winning" = unlist(tempWinNoSwitch),
                        "Losing" = unlist(tempLoseNoSwitch))
 # Plot the Probability of winning after switching doors
-boxplot(switch$Winning,
-        main = "Probability of winning after switching doors",
-        xlab = "Winning with switching (1000 iterations)",
-        ylab = "Probability",
-        col = "orange")
+boxplot(
+  switch$Winning,
+  main = "Probability of winning after switching doors",
+  xlab = "Winning with switching (1000 iterations)",
+  ylab = "Probability",
+  col = "orange"
+)
 # Plot the Probability of losing after switching the door mid-way
-boxplot(switch$Losing,
-        main = "Probability of losing after switching doors",
-        xlab = "Losing with switching (1000 iterations)",
-        ylab = "Probability",
-        col = "orange")
+boxplot(
+  switch$Losing,
+  main = "Probability of losing after switching doors",
+  xlab = "Losing with switching (1000 iterations)",
+  ylab = "Probability",
+  col = "orange"
+)
 # Plot the Probability of winning without switching the door
-boxplot(noSwitch$Winning,
-        main = "Probability of winning without switching doors",
-        xlab = "Winning without switching (1000 iterations)",
-        ylab = "Probability",
-        col = "orange")
+boxplot(
+  noSwitch$Winning,
+  main = "Probability of winning without switching doors",
+  xlab = "Winning without switching (1000 iterations)",
+  ylab = "Probability",
+  col = "orange"
+)
 # Plot the Probability of losing without switching the door
-boxplot(noSwitch$Losing,
-        main = "Probability of losing without switching doors",
-        xlab = "Losing without switching (1000 iterations)",
-        ylab = "Probability",
-        col = "orange")
-
+boxplot(
+  noSwitch$Losing,
+  main = "Probability of losing without switching doors",
+  xlab = "Losing without switching (1000 iterations)",
+  ylab = "Probability",
+  col = "orange"
+)
